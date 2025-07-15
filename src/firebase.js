c454512c9eeb9319836a3d69ed62b09a1c6cff47
jannatulMahiyaMutasha <mutashamahiya678@gmail.com>
2025-07-15 21:04:18 +0600

    firebase add

--- src/firebase.js
+++ src/firebase.js
@@ -23,13 +23,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDe1j3LTQM0WLnyvibD21p2kU77QhVjuDQ",
-  authDomain: "subscription-box-fd605.firebaseapp.com",
-  projectId: "subscription-box-fd605",
-  storageBucket: "subscription-box-fd605.firebasestorage.app",
-  messagingSenderId: "247365881797",
-  appId: "1:247365881797:web:f1c57c8fb6445ed22bc5ac",
-  measurementId: "G-MLWRSPHRQP",
+  apiKey: "AIzaSyB20AKs8RYQYSDLZ6z3-DGN_oPMKrOPCYQ",
+  authDomain: "mallon-multi-vendor.firebaseapp.com",
+  projectId: "mallon-multi-vendor",
+  storageBucket: "mallon-multi-vendor.firebasestorage.app",
+  messagingSenderId: "312124466381",
+  appId: "1:312124466381:web:9afa485c4ea2215de6b584"
 };
 
 const app = initializeApp(firebaseConfig);
