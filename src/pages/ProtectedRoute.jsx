1a34f693b3c1f52aa3efe13624d2e3cccae7be7b
jannatulMahiyaMutasha <mutashamahiya678@gmail.com>
2025-07-15 21:57:00 +0600

    add pages file add code

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -3,7 +3,6 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children, user, token }) => {
   if (!user || !token) {
-    // Redirect to login if no token or user
     return <Navigate to="/login" />;
   }
 
