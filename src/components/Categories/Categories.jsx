6178fae56cf91cada5cde7f6ab109a4b03d191c4
jannatulMahiyaMutasha <mutashamahiya678@gmail.com>
2025-08-17 11:59:18 +0600

    change commit

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom"; // ✅ Import Link
+import { Link } from "react-router-dom"; 
 
 export default function CategoryCardSection() {
   const [categories, setCategories] = useState([]);
